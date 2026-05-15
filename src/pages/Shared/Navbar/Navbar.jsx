3fafc07b924afc70cf543107e1c25871e7f4a6e7
jahidul islam <jahidul20673@gmail.com>
2026-05-15 20:50:39 +0600

    Fix duplicate 'Be a Raider' link in Navbar component

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
         <li><NavLink to='aboutUs'>About Us</NavLink></li>
         <li><NavLink to='coverage'>coverage</NavLink></li>
         <li><NavLink to='raider'>Be a Raider</NavLink></li>
+        <li><NavLink to='raider'</NavLink></li>
     </>
 
     const { user, logOut } = UseAuth();
